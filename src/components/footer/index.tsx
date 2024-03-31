00d5e4affacfa9581e4f12a5ecd693ab5aa8da94
Thiago <tmg3.mn@gmail.com>
2024-03-31 20:39:37 -0300

    Feat: Create container div for sections

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,40 +6,42 @@ import { Facebook, Twitter } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <section className="bg-dark-green flex flex-col gap-5 items-center justify-center py-10 text-white">
-      <div className="text-white flex flex-col gap-5 items-center sm:flex-row sm:justify-between sm:w-full sm:px-5">
-        <div className="flex flex-col gap-2 sm:gap-10">
-          <Image
-            src={LogoImage}
-            alt="myteam Logo Image"
-            width={150}
-            height={100}
-          />
-          <ul className="flex justify-between gap-5  body-1">
-            <li>home</li>
-            <li>about</li>
+    <section className="bg-dark-green py-10 text-white">
+      <div className="main-container flex-col gap-5 lg:container lg:max-w-screen-xl">
+        <div className="text-white flex flex-col gap-5 items-center sm:flex-row sm:justify-between sm:w-full ">
+          <div className="flex flex-col gap-2 sm:gap-10">
+            <Image
+              src={LogoImage}
+              alt="myteam Logo Image"
+              width={150}
+              height={100}
+            />
+            <ul className="flex justify-between gap-5  body-1">
+              <li>home</li>
+              <li>about</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-2 justify-center items-center sm:items-end">
+            <p className="body-1">987 Hillcrest Lane</p>
+            <p className="body-1">Irvine, CA</p>
+            <p className="body-1">California 92714</p>
+            <p className="body-1">Call us: 949-833-7432</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:w-full">
+          <ul className="flex gap-2 justify-center">
+            <li>
+              <Facebook />
+            </li>
+            <li>
+              <Twitter />
+            </li>
+            <li>
+              <Image src={Pinterest} alt="Pinterest Logo" />
+            </li>
           </ul>
+          <p className="body-2">Copyright 2024. All Rights Reserved</p>
         </div>
-        <div className="flex flex-col gap-2 justify-center items-center sm:items-end">
-          <p className="body-1">987 Hillcrest Lane</p>
-          <p className="body-1">Irvine, CA</p>
-          <p className="body-1">California 92714</p>
-          <p className="body-1">Call us: 949-833-7432</p>
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:w-full sm:px-5">
-        <ul className="flex gap-2 justify-center">
-          <li>
-            <Facebook />
-          </li>
-          <li>
-            <Twitter />
-          </li>
-          <li>
-            <Image src={Pinterest} alt="Pinterest Logo" />
-          </li>
-        </ul>
-        <p className="body-2">Copyright 2024. All Rights Reserved</p>
       </div>
     </section>
   );
--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -5,21 +5,23 @@ import { ContactButton } from "../contact-button";
 
 export const Navigation = () => {
   return (
-    <div className="flex justify-between p-5 bg-midnight-green items-center">
-      <div className="flex gap-5 items-center">
-        <Image
-          src={LogoImage}
-          alt="myteam Logo Image"
-          width={150}
-          height={100}
-        />
-        <ul className="hidden sm:flex gap-2 body-1 text-white">
-          <li>home</li>
-          <li>about</li>
-        </ul>
+    <div className="p-5 bg-midnight-green">
+      <div className="main-container lg:container lg:max-w-screen-xl">
+        <div className="flex gap-5 items-center">
+          <Image
+            src={LogoImage}
+            alt="myteam Logo Image"
+            width={150}
+            height={100}
+          />
+          <ul className="hidden sm:flex gap-2 body-1 text-white">
+            <li>home</li>
+            <li>about</li>
+          </ul>
+        </div>
+        <Menu className="block sm:hidden" color="white" />
+        <ContactButton />
       </div>
-      <Menu className="block sm:hidden" color="white" />
-      <ContactButton />
     </div>
   );
 };
