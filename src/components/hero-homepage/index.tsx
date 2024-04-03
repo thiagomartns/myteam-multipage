e30bc6be9f7bcb193b997bdfaf1d2ab31cdec971
Thiago <tmg3.mn@gmail.com>
2024-04-03 09:52:17 -0300

    Feat: Hero home page section

---
+++ src/components/hero-homepage/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const HeroHomepage = () => {
+  return (
+    <section className="py-32 bg-midnight-green">
+      <div className="lg:container lg:max-w-screen-xl">
+        <div className="flex flex-col gap-8 items-center px-5 md:px-32 lg:flex-row lg:px-0 lg:items-end lg:justify-between">
+          <h1 className="text-center text-white text-5xl font-bold w-[9ch] md:text-6xl lg:text-7xl lg:text-left lg:w-[12ch] xl:w-[9ch] ">
+            Find the best <span className="text-light-coral">talent</span>
+          </h1>
+          <p className="text-white text-center text-xl font-semibold lg:text-left lg:w-96 xl:w-[550px]">
+            Finding the right people and building high performing teams can be
+            hard. Most companies aren’t tapping into the abundance of global
+            talent. We’re about to change that.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/info-homepage/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import IconPerson from "@/assets/img/icon-person.svg";
+import IconCog from "@/assets/img/icon-cog.svg";
+import IconChart from "@/assets/img/icon-chart.svg";
+import Image from "next/image";
+
+export const InfoHomepage = () => {
+  const items = [
+    {
+      id: 1,
+      title: "Experienced Individuals",
+      info: "Our network is made up of highly experienced professionals who are passionate about what they do.",
+      img: IconPerson,
+    },
+    {
+      id: 2,
+      title: "Easy to Implement",
+      info: "Our processes have been refined over years of implementation meaning our teams always deliver.",
+      img: IconCog,
+    },
+    {
+      id: 3,
+      title: "Enhanced Productivity",
+      info: "Our customized platform with in-built analytics helps you manage your distributed teams.",
+      img: IconChart,
+    },
+  ];
+
+  return (
+    <section className="py-32 bg-dark-green">
+      <div className="container flex flex-col gap-10 lg:max-w-screen-xl lg:flex-row lg:justify-between lg:items-start">
+        <h1 className=" text-white text-5xl w-[12ch] font-bold md:w-full lg:text-5xl lg:w-[25ch] xl:w-[15ch]">
+          Build & manage distributed teams like no one else.
+        </h1>
+        <ul className="lg:flex lg:flex-col lg:gap-10">
+          {items.map((item) => (
+            <li
+              className="flex flex-col gap-5 items-center py-8 md:flex-row lg:items-start lg:py-0"
+              key={item.id}
+            >
+              <Image
+                width={96}
+                height={24}
+                alt={`Icon-${item.title}`}
+                src={item.img}
+              />
+              <div className="gap-5 flex flex-col ">
+                <h3 className="text-light-coral text-xl font-bold text-center md:text-left">
+                  {item.title}
+                </h3>
+                <p className="text-white text-center font-semibold md:text-left xl:w-[50ch]">
+                  {item.info}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
